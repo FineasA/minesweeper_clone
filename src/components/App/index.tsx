63fa7104b6dd64b39c0f0ca72fbf0f51ae3fc6c0
Fineas Anton <fineas.anton@gmail.com>
2021-03-03 12:26:46 -0600

    added memory cleanup

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,21 +3,29 @@ import "./App.scss";
 import NumberDisplay from "../NumberDisplay";
 import Button from "../Button";
 import { generateCells } from "../../utils";
-import { Face } from "../../types";
+import { Face, Cell } from "../../types";
 
 const App: React.FC = () => {
-  const [cells, setCells] = useState(generateCells());
-  const [face, setFace] = useState(Face.smile);
+  const [cells, setCells] = useState<Cell[][]>(generateCells());
+  const [face, setFace] = useState<Face>(Face.smile);
+  const [time, setTime] = useState<number>(0);
+  const [live, setLive] = useState<boolean>(false);
 
   useEffect(() => {
-    const handleMouseDown = () => {
+    const handleMouseDown = (): void => {
       setFace(Face.oh);
     };
-    const handleMouseUp = () => {
+    const handleMouseUp = (): void => {
       setFace(Face.smile);
     };
     window.addEventListener("mousedown", handleMouseDown);
     window.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      //cleanup to save memory
+      window.removeEventListener("mousedown", handleMouseDown);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
   }, []);
 
   const renderCells = (): React.ReactNode => {
@@ -43,7 +51,7 @@ const App: React.FC = () => {
             {face}
           </span>
         </div>
-        <NumberDisplay value={23} />
+        <NumberDisplay value={time} />
       </div>
       <div className="Body">{renderCells()}</div>
     </div>
